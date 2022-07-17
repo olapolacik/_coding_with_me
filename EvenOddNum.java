d3d9ab80b67524bbff7da06715c43b996b385a9d
Aleksandra Połacik <polacik.ola@gmail.com>
2022-07-17 15:05:19 +0200

    EvenOddNum

--- EvenOddNum.java
+++ EvenOddNum.java
@@ -8,10 +8,11 @@ public class EvenOddNum {
         Scanner sc = new Scanner(System.in);
         System.out.println("Podaj liczbe: ");
         int num = sc.nextInt();
-        if(num % 2 == 0){
+
+        if(num % 2 ==0){
             System.out.println("Podana liczba jest parzysta");
         }
-        else{
+        else {
             System.out.println("Podana liczba jest nieparzysta");
         }
     }
